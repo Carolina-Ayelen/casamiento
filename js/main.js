ed91c8fad02fc1048e951520af47ce607d0be1d3
Carolina-Ayelen <70728509+Carolina-Ayelen@users.noreply.github.com>
2020-10-02 19:12:55 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function mostrarDiv(){
+  var divOculto=document.getElementById('idoculto');
+
+  divOculto.style.display="block";
+}
+
+function ocultarDiv(){
+  var divOculto=document.getElementById('idoculto');
+
+  divOculto.style.display="none";
+}
